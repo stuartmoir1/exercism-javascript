a5c9f45abfa2a038b09c2d338966c5f4f55b2f48
Stuart Moir <stuart.a.moir@live.com>
2020-11-15 13:19:28 +0000

    Adds resistor-color

---
+++ resistor-color/exercism.js
@@ -0,0 +1,21 @@
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white"
+];
+
+const colorCode = (color) => {
+  return COLORS.indexOf(color);
+};
+
+module.exports = {
+  COLORS,
+  colorCode
+};
