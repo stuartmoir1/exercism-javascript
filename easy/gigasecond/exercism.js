dc9bbe2034dc9abcf792e3626ae40de588666887
Stuart Moir <stuart.a.moir@live.com>
2021-01-13 19:55:41 +0000

    Refactors gigasecond

--- easy/gigasecond/exercism.js
+++ easy/gigasecond/exercism.js
@@ -1,7 +1,5 @@
 const gigasecond = (date) => {
-  const ms1 = date.getTime(date) / 1000;
-  const ms2 = Math.pow(10, 9);
-  return new Date((ms1 + ms2) * 1000);
+  return new Date(date.getTime() + 10e11);
 };
 
 module.exports = {
